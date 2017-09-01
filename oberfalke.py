d536f71c6eb489885739ca58d408f4b2f9a95c42
Martin W <mart-w@users.noreply.github.com>
2017-09-01 12:46:21 +0200

    Translate code to English for better readability

--- oberfalke.py
+++ oberfalke.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
 
-# Falkenbot -- die softwaregewordene Inkarnation unserer gelieben Falken.
+# Falkenbot -- a software incarnation of our beloved falcons.
 
 import discord
 import asyncio
 import datetime
 
-# Lese API-Token ein.
-with open("token.txt", "r") as tokendatei:
-    api_token = tokendatei.readline().rstrip()
+# Read API token from token file.
+with open("token.txt", "r") as tokenfile:
+    api_token = tokenfile.readline().rstrip()
 
-# Client-Klasse mit Event-Listenern
-class Oberfalkenclient(discord.Client):
-    # Vergleiche send_message(). Simuliert Tippen durch den Bot.
+# Client class with event listeners
+class Oberfalke_client(discord.Client):
+    # Similar to send_message(), but simulates actual typing
     async def type_message(self, destination, content=None, tts=False, embed=None):
-        # Berechne simulierte Tippzeit in Sekunden aus Länge der Nachricht.
+        # Calculate typing_time based on the length of content
         if not content:
-            tippzeit = 1 # Keine Nachricht; nehme 1 für eingebettete Inhalte an.
+            typing_time = 1 # No text content; assume 1 for embedded content
         else:
-            tippzeit = len(str(content)) / 30
-            if tippzeit > 10:
-                tippzeit = 10
+            typing_time = len(str(content)) / 30
+            if typing_time > 10:
+                typing_time = 10
 
         await self.send_typing(destination)
-        await asyncio.sleep(tippzeit)
+        await asyncio.sleep(typing_time)
         return await self.send_message(destination, content=content, tts=tts, embed=embed)
 
-    # Event-Listener:
+    # Event listeners:
     async def on_ready(self):
         print("Verbunden mit %d Servern:" % (len(client.servers)))
         for server in client.servers:
             print("%s -- %s" % (server.id, server.name))
 
-# Initialisiere Client-Objekt.
-client = Oberfalkenclient()
+# Initialise client object.
+client = Oberfalke_client()
 
-# Starte Bot-Session.
+# Start bot session.
 client.run(api_token)
 
 # MIT License
